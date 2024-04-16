108a94c8b27ecad64bbc292d61c05ec462a59e40
Vadim <117563191+VadimQA2021@users.noreply.github.com>
2024-04-16 20:01:37 +0700

    Create 8_task_even_and_odd_numbers.cs

---
+++ 8_task_even_and_odd_numbers.cs
@@ -0,0 +1,31 @@
+using System;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		//Подсчитывать количество четных и нечетных чисел в диапозоне, указанном пользователем
+		int count = 0;
+		int sumChetnoeChislo = 0;
+		int sumNeChetnoeChislo = 0;
+		const string chetnoeChislo = "Chetnoe Chislo";
+		const string neChetnoeChislo = "Nechetnoe Chislo";
+		while (count < 10)
+		{
+			count++;
+			Console.WriteLine(count);
+			if(count % 2 == 0)
+			{
+			sumChetnoeChislo++;
+			}
+			else
+			{
+			sumNeChetnoeChislo++;
+			}
+		}
+		Console.WriteLine(chetnoeChislo);
+		Console.WriteLine(sumChetnoeChislo);
+		Console.WriteLine(neChetnoeChislo);
+		Console.WriteLine(sumNeChetnoeChislo);
+	}
+}
