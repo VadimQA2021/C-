4d35adea5a92580812e094d8f8488e0e8c0a4b19
Vadim <117563191+VadimQA2021@users.noreply.github.com>
2024-04-18 21:39:42 +0700

    Create 9_task.cs

---
+++ 9_task.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		//Напишите программу, которая выводит квадраты нечетных целых чисел от 1 до предела который вводит пользователь.
+		
+for (int i = 1; i <= 11; i++)
+    if (i % 2 == 0)
+	Console.WriteLine();
+	else Console.WriteLine(i * i);
+	}
+}
