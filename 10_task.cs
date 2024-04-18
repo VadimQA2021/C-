7e370793118c1bf2a435607e67de23f940c0060d
Vadim <117563191+VadimQA2021@users.noreply.github.com>
2024-04-18 22:24:09 +0700

    Create 10_task.cs

---
+++ 10_task.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program
+{
+	public static void Main(string[] args)
+		
+	{
+		//Найдите сумму  1+2+3+…+n, где число n вводится пользователем с клавиатуры.
+
+	//int n;
+	
+	Console.WriteLine(1 + 2 + 3 + (Convert.ToInt32(Console.ReadLine())));
+	
+	}
+}
