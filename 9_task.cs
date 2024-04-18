06f03b61be19179a3e848d1adc6b8e1a11fd4b4a
Vadim <117563191+VadimQA2021@users.noreply.github.com>
2024-04-18 23:01:22 +0700

    Update 9_task.cs

--- 9_task.cs
+++ 9_task.cs
@@ -7,8 +7,7 @@ public class Program
 		//Напишите программу, которая выводит квадраты нечетных целых чисел от 1 до предела который вводит пользователь.
 		
 for (int i = 1; i <= 11; i++)
-    if (i % 2 == 0)
-	Console.WriteLine();
-	else Console.WriteLine(i * i);
+    if (i % 2 != 0)
+	Console.WriteLine(i * i);
 	}
 }
