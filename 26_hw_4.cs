3684ee2ba2956811333714e2ffb0d8d688aa4bfc
Vadim <117563191+VadimQA2021@users.noreply.github.com>
2024-04-25 20:13:14 +0700

    Update 26_hw_4.cs

--- 26_hw_4.cs
+++ 26_hw_4.cs
@@ -1,13 +1,30 @@
 using System;
-using System.Linq;
- 
-public class Example
+					
+public class Program
 {
-    public static void Main()
-    {	//Найти наименьшее и наибольшее число в массиве
-		int[] myArray = {2, 6, 5, 5 }; 
- 
-        Console.WriteLine("Minimum number is " + myArray.Min());
-        Console.WriteLine("Maximum number is " + myArray.Max());
-    }
+	public static void Main()
+	{
+		// Найти сумму четных чисел в массиве
+		
+		int[] myArray;
+		Console.WriteLine("Determine the range length");
+		int lenghtRange = Convert.ToInt32(Console.ReadLine());
+		
+		myArray = new int[lenghtRange];
+		
+		
+		//В этом цикле проиходит ввод данных
+		for(int i = 0; i < lenghtRange; i++)
+		myArray[i] = Convert.ToInt32(Console.ReadLine());
+		
+		int minValue = myArray[0];
+			
+		//В этом цикле проиходит сравнение с минимальных значением
+		for(int i = 1; i < lenghtRange; i++)
+		{
+		if(myArray[i] < minValue)
+			minValue = myArray[i];		
+		}
+		Console.WriteLine(minValue);
+	}
 }
