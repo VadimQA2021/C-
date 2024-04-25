8568145bd4e0a0e9c62f79cc4a388271e6704e95
Vadim <117563191+VadimQA2021@users.noreply.github.com>
2024-04-25 14:32:10 +0700

    Update 26_hw_1.cs

--- 26_hw_1.cs
+++ 26_hw_1.cs
@@ -8,15 +8,9 @@ public class Program
 		
 		int[] myArray;
 		myArray = new int[5];
-		myArray[0] = Convert.ToInt32(Console.ReadLine());
-		myArray[1] = Convert.ToInt32(Console.ReadLine());
-		myArray[2] = Convert.ToInt32(Console.ReadLine());
-		myArray[3] = Convert.ToInt32(Console.ReadLine());
-		myArray[4] = Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine(myArray[0]);
-		Console.WriteLine(myArray[1]);
-		Console.WriteLine(myArray[2]);
-		Console.WriteLine(myArray[3]);
-		Console.WriteLine(myArray[4]);
+		for(int i = 0; i < myArray.Length; i++)
+		{
+		Convert.ToInt32(Console.ReadLine());
+		}
 	}
 }
