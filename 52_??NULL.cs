e974649cefc5caba929ae5801bfedba95dc50b8f
Vadim <117563191+VadimQA2021@users.noreply.github.com>
2024-05-29 15:53:46 +0600

    Create 52_??NULL.cs

---
+++ 52_??NULL.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       //ОПЕРАТОР ОБЪЕДИНЕНИЯ С NULL
+		string str = null;
+		Console.WriteLine(str ?? "нет данных");
+		
+	}
+}
